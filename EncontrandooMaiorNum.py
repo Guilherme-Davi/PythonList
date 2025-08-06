4d21717241c7edfe77b550bd31fb8535792b54ca
Guilherme Davi <guilherme.macedo3006@gmail.com>
2025-08-06 14:19:20 -0300

    solucao concluida encontrar maior numero

---
+++ EncontrandooMaiorNum.py
@@ -0,0 +1,10 @@
+def encontrar_maior_numero(lista):
+    maior = lista[0]
+    for numero in lista:
+        if numero > maior:
+            maior = numero
+    print(f"O maior número da lista é: {maior}")
+
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+encontrar_maior_numero(numeros)
